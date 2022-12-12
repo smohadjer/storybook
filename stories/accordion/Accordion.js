0208006fc9b64e128e64c764f81f01b42298bb89
Saeid Mohadjer <saeid@fastmail.fm>
2022-12-12 13:51:48 +0100

    adds open state

--- stories/accordion/Accordion.js
+++ stories/accordion/Accordion.js
@@ -1,15 +1,20 @@
 import './accordion.scss';
 import Accordion from './AccordionClass.js';
 
-export const createAccordion = ({}) => {
+export const createAccordion = ({
+  label,
+  content,
+  open = false,
+}) => {
   const details = document.createElement('details');
   details.className = 'accordion';
-  const markup = `<summary>Lorem ipsum dolor sit</summary>
-  <div class="accordion__content">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, necessitatibus hic earum ipsum labore tempore nisi ipsa dicta est, modi, ipsam officia rerum? Nulla exercitationem molestiae, nesciunt ducimus possimus necessitatibus.</p>
-    <p>Ipsa dicta est, modi, ipsam officia rerum? Nulla exercitationem molestiae, nesciunt ducimus possimus necessitatibus.</p>
-  </div>`
-
+  if (open) {
+    details.setAttribute('open', 'open');
+  }
+  const markup = `<summary>${label}</summary>
+    <div class="accordion__content">
+      ${content}
+    </div>`
   details.insertAdjacentHTML('afterbegin', markup);
   new Accordion(details);
   return details;
--- stories/accordion/Accordion.stories.js
+++ stories/accordion/Accordion.stories.js
@@ -7,16 +7,26 @@ export default {
   },
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {
-    onLogin: { action: 'onLogin' },
-    onLogout: { action: 'onLogout' },
-    onCreateAccount: { action: 'onCreateAccount' },
+
   },
 };
 
 const Template = (args) => createAccordion(args);
 
-export const Primary = Template.bind({});
-Primary.args = {
-  size: 'large',
-  label: 'Button',
+export const Default = Template.bind({});
+Default.args = {
+  open: false,
+  label: 'Hello World',
+  content: `<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, necessitatibus hic earum ipsum labore tempore nisi ipsa dicta est, modi, ipsam officia rerum? Nulla exercitationem molestiae, nesciunt ducimus possimus necessitatibus.</p>
+  <p>Ipsa dicta est, modi, ipsam officia rerum? Nulla exercitationem molestiae, nesciunt ducimus possimus necessitatibus.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, necessitatibus hic earum ipsum labore tempore nisi ipsa dicta est, modi, ipsam officia rerum? Nulla exercitationem molestiae, nesciunt ducimus possimus necessitatibus.</p>`
+};
+
+export const accordionOpen = Template.bind({});
+accordionOpen.args = {
+  open: true,
+  label: 'Accordion is initially open',
+  content: `<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, necessitatibus hic earum ipsum labore tempore nisi ipsa dicta est, modi, ipsam officia rerum? Nulla exercitationem molestiae, nesciunt ducimus possimus necessitatibus.</p>
+  <p>Ipsa dicta est, modi, ipsam officia rerum? Nulla exercitationem molestiae, nesciunt ducimus possimus necessitatibus.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, necessitatibus hic earum ipsum labore tempore nisi ipsa dicta est, modi, ipsam officia rerum? Nulla exercitationem molestiae, nesciunt ducimus possimus necessitatibus.</p>`
 };
